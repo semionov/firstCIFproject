d61a391f0909d10195c95d5c136a1ab1495ef127
mati <mati@DESKTOP-K5660H8>
2021-04-22 09:31:22 +0200

    first commit

---
+++ First CIF project/src/one/Main.java
@@ -0,0 +1,32 @@
+package one;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner reader = new Scanner (System.in);
+		
+		System.out.println("Tell me your user name:");
+		String userName = reader.nextLine();
+		
+		System.out.println("Tell me your type of user:");
+		String userType = reader.nextLine();
+		
+		System.out.println("Tell me the amount of your buy:");
+		int userBuy = reader.nextInt();
+		
+		System.out.println("User/n");
+		System.out.println("-----------------------");
+		System.out.println("User name: " + userName);
+		System.out.println("User Type: " + userType);
+		System.out.println(":");
+		
+	
+	}
+
+}
+
